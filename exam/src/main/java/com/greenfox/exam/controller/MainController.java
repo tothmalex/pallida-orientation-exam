f3a4b03905203a1c571369bd2b5627068454c92d
Alex Toth <drive_alex@hotmail.com>
2017-11-17 13:06:38 +0100

    final version

--- exam/src/main/java/com/greenfox/exam/controller/MainController.java
+++ exam/src/main/java/com/greenfox/exam/controller/MainController.java
@@ -18,13 +18,15 @@ public class MainController {
 
     @RequestMapping({"","/search"})
     public String list( Model model, @RequestParam(required = false) String search) {
-        if (search != null || search.matches("^.*[^a-zA-Z0-9 ].*$") || search.length() < 7) {
+        if (search.matches("^.*[^a-zA-Z0-9 ].*$") || search.length() < 7) {
             model.addAttribute("cars", carRepo.findAllByPlateIsLike("%" + search + "%"));
+        }
+        else if (search == null) {
+            model.addAttribute("cars", carRepo.findAll());
         }
         else {
             model.addAttribute("errorMessage", "Sorry, the submitted licence plate is not valid");
-        }
-        return "searchform";
+            return "searchform";
     }
 
     @GetMapping("/police")
